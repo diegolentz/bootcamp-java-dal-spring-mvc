8df969c49d234fd3d5623cd3e8707d4ddaaa18cf
DiegoLentz <diegoolentz@gmail.com>
2022-09-15 13:07:27 -0300

    spring clase 1

--- src/main/java/ar/com/educacionit/SpringMvcApplication.java
+++ src/main/java/ar/com/educacionit/SpringMvcApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("tuki");
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
 
