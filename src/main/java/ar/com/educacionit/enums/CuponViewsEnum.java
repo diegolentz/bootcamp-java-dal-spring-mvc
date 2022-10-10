e6e6e5a488feec008139cf6afc145715b41689a0
DiegoLentz <diegoolentz@gmail.com>
2022-10-10 15:55:00 -0300

    deploy

--- src/main/java/ar/com/educacionit/enums/CuponViewsEnum.java
+++ src/main/java/ar/com/educacionit/enums/CuponViewsEnum.java
@@ -3,16 +3,16 @@ package ar.com.educacionit.enums;
 
 public enum CuponViewsEnum {
 
-	LIST("/cupon/list"),
-	EDIT("/cupon/edit"),
-	NEW("/cupon/new"),
-	LIST_REDIRECT("redirect:/cupon/list"),
-	;
+	LIST("cupon/list"),
+	EDIT("cupon/edit"),
+	NEW("cupon/new"),
+	LIST_REDIRECT("redirect:/cupon/list");
+
 	
 	private String view;
-	
+
 	private CuponViewsEnum(String view) {
-		this.view = view;			
+		this.view = view;
 	}
 
 	public String getView() {
