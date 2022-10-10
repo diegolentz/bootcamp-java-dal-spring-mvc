9b84e4c394db3424af91b7cefa2083b103675208
DiegoLentz <diegoolentz@gmail.com>
2022-10-10 15:40:06 -0300

    deploy

--- src/main/java/ar/com/educacionit/enums/SocioViewsEnum.java
+++ src/main/java/ar/com/educacionit/enums/SocioViewsEnum.java
@@ -1,22 +1,23 @@
 
+
 package ar.com.educacionit.enums;
 
-public enum SocioViewsEnum {
+	public enum SocioViewsEnum {
 
-	LIST("/socio/list"),
-	EDIT("/socio/edit"),
-	NEW("/socio/new"),
-	LIST_REDIRECT("redirect:/socio/list"),
-	;
-	
-	private String view;
-	
-	private SocioViewsEnum(String view) {
-		this.view = view;			
-	}
+		LIST("socio/list"),
+		EDIT("socio/edit"),
+		NEW("socio/new"),
+		LIST_REDIRECT("redirect:/socio/list"),
+		;
+		
+		private String view;
+		
+		private SocioViewsEnum(String view) {
+			this.view = view;			
+		}
 
-	public String getView() {
-		return view;
-	}
-	
+		public String getView() {
+			return view;
+		}
+		
 }
